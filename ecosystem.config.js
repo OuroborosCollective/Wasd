444bfc825dbf3806245965817f46081222269ba2
Ouro <projectouroboroscollective@gmail.com>
2026-04-29 21:52:39 +0200

    Architect Deploy: Module Update (3 files)

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: 'areloria-app',
+      script: 'index.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
