3b560da65106a50001d23befbb8f277c5ba80c3b
Ouro <projectouroboroscollective@gmail.com>
2026-05-01 04:36:32 +0200

    fix: convert tailwind.config.js to ESM

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "../packages/shared/**/*.{js,ts,jsx,tsx}"
+    "../shared/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
