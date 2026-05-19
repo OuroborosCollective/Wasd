734ec7134cd89cee02e4797da8bba23527ce4ed8
Ouro <projectouroboroscollective@gmail.com>
2026-05-19 02:19:47 +0200

    fix(2d): mount login gate before pixi client

--- apps/client-2d/src/main.tsx
+++ apps/client-2d/src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { CyberZenLoginGate } from "./CyberZenLoginGate";
+import "./theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <CyberZenLoginGate>
+      <App />
+    </CyberZenLoginGate>
   </React.StrictMode>
 );
