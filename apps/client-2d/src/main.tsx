32c59b489ccc31e787cd3ff98f17abe6509dc0fe
Ouro <projectouroboroscollective@gmail.com>
2026-05-19 03:23:46 +0200

    fix(ui): mount cyberzen isometric 2d shell

--- apps/client-2d/src/main.tsx
+++ apps/client-2d/src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
 import { CyberZenLoginGate } from "./CyberZenLoginGate";
+import { CyberZenIsoApp } from "./CyberZenIsoApp";
 import "./theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CyberZenLoginGate>
-      <App />
+      <CyberZenIsoApp />
     </CyberZenLoginGate>
   </React.StrictMode>
 );
