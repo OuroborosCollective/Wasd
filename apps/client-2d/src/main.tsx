d0201b2e830c98d0754a7a160832ac1e34021563
Ouro <projectouroboroscollective@gmail.com>
2026-05-22 01:25:41 +0200

    feat(client-2d): mount live reality bridge

--- apps/client-2d/src/main.tsx
+++ apps/client-2d/src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { CyberZenLoginGate } from "./CyberZenLoginGate";
 import { CyberZenIsoApp } from "./CyberZenIsoApp";
+import { LiveRealityBridge } from "./LiveRealityBridge";
 import "./theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CyberZenLoginGate>
       <CyberZenIsoApp />
+      <LiveRealityBridge />
     </CyberZenLoginGate>
   </React.StrictMode>
 );
