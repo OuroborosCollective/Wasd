bbc2957f761169c6c411a6066f13943364b76b09
Ouro <projectouroboroscollective@gmail.com>
2026-05-22 02:31:35 +0200

    feat(client-2d): mount mobile controls and live css

--- apps/client-2d/src/main.tsx
+++ apps/client-2d/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { CyberZenLoginGate } from "./CyberZenLoginGate";
 import { CyberZenIsoApp } from "./CyberZenIsoApp";
 import { LiveRealityBridge } from "./LiveRealityBridge";
+import { MobileMovePad } from "./MobileMovePad";
 import "./theme.css";
+import "./liveReality.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CyberZenLoginGate>
       <CyberZenIsoApp />
       <LiveRealityBridge />
+      <MobileMovePad />
     </CyberZenLoginGate>
   </React.StrictMode>
 );
