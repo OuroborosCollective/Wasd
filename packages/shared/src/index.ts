f95facebd8a61db9fb95b6b945002148bcbba8ed
Ouro <projectouroboroscollective@gmail.com>
2026-05-30 09:09:20 +0200

    feat(shared): export world director

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,5 +2,6 @@ export * from './math';
 export * from './constants';
 export * from './types';
 export * from './utils';
+export * from './world/index.js';
 export * from './GuildSovereigntyEngine.js';
 export * from './theme/ThemeEngine.js';
