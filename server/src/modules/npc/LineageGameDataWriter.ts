798d2a0cd4a040f5d9420fe698a4c6c6520d7d39
Ouro <projectouroboroscollective@gmail.com>
2026-06-14 23:05:30 +0200

    chore: touch lineage writer

--- server/src/modules/npc/LineageGameDataWriter.ts
+++ server/src/modules/npc/LineageGameDataWriter.ts
@@ -1,9 +1,1 @@
-export interface NpcLineageRecordSink {
-  recordNpcLineage(value: unknown): void;
-}
-
-export class NpcLineageGameDataWriter implements NpcLineageRecordSink {
-  recordNpcLineage(_value: unknown): void {
-    return;
-  }
-}
+export const lineageGameDataWriterReady = true;
