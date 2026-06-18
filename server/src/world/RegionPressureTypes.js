94c94215c4ed31f781cf562d9d55b5331f7d313b
Ouro <projectouroboroscollective@gmail.com>
2026-06-18 17:26:28 +0200

    fix(world): wrap region pressure constants

--- server/src/world/RegionPressureTypes.js
+++ server/src/world/RegionPressureTypes.js
@@ -1,3 +1,17 @@
 export const REGION_PRESSURE_KAPPA = 1000;
-export const REGION_LOD_TIERS = Object.freeze(["observed_chunk", "near_chunk", "region_summary", "sleeping_region"]);
-export const REGION_PRESSURE_FIELDS = Object.freeze(["resourcePressurePerMille", "migrationPressurePerMille", "tradePressurePerMille", "threatPressurePerMille", "faminePressurePerMille", "warPressurePerMille"]);
+
+export const REGION_LOD_TIERS = Object.freeze([
+  "observed_chunk",
+  "near_chunk",
+  "region_summary",
+  "sleeping_region",
+]);
+
+export const REGION_PRESSURE_FIELDS = Object.freeze([
+  "resourcePressurePerMille",
+  "migrationPressurePerMille",
+  "tradePressurePerMille",
+  "threatPressurePerMille",
+  "faminePressurePerMille",
+  "warPressurePerMille",
+]);
