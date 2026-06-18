83b8f57f36cfc10cb892c31c054108c4b593307c
Ouro <projectouroboroscollective@gmail.com>
2026-06-18 17:22:54 +0200

    feat(world): add region pressure contract constants

--- server/src/world/RegionPressureTypes.js
+++ server/src/world/RegionPressureTypes.js
@@ -1,1 +1,3 @@
 export const REGION_PRESSURE_KAPPA = 1000;
+export const REGION_LOD_TIERS = Object.freeze(["observed_chunk", "near_chunk", "region_summary", "sleeping_region"]);
+export const REGION_PRESSURE_FIELDS = Object.freeze(["resourcePressurePerMille", "migrationPressurePerMille", "tradePressurePerMille", "threatPressurePerMille", "faminePressurePerMille", "warPressurePerMille"]);
